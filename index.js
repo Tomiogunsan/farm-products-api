a1e0bf2711e5b297bf65cc8e49a097fe7d1f1cc2
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-08-29 17:33:44 +0100

    ch: display all product

--- index.js
+++ index.js
@@ -14,13 +14,36 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
 });
 
 //server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%NOT_ORGANIC%}/g, product.organic ? "" : "not-organic");
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct= fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-// const productData = JSON.parse(data);
+ const productData = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is overview");
+     res.writeHead(200, {
+       "Content-type": "text/html",
+     });
+
+     const card = productData.map(el => replaceTemplate(tempCard, el)).join('');
+     const outPut = tempOverview.replace('{%PRODUCT_CARDS%}', card);
+    res.end(outPut);
   } else if (pathName === "/product") {
     res.end("This is products");
   } else if (pathName === "/api") {
