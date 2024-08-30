b1e512492c98049c0fde10a5f6e6f60df10b0a52
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-08-30 14:14:34 +0100

    installed nodemon and slugify

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
+
 
 // file
 const res = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -32,6 +34,8 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productData = JSON.parse(data);
+
+const slug = productData.map(el => slugify(el.productName, {lower:true}))
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
